13f11b1c56555df7535532fc1dc2262df399c01f
Mohammed Arif <mohammedarifsn2@gmail.com>
2025-04-02 19:30:01 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -1,15 +1,16 @@
+import streamlit as st
 from langchain_groq import ChatGroq
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+# Load API key from Streamlit secrets
+groq_api_key = st.secrets["GROQ_API_KEY"]
 
+# Initialize Groq Chat Model
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="llama-3.3-70b-versatile")
+
+# Test the API
 if __name__ == "__main__":
-    response = llm.invoke("Two most important ingradient in samosa are ")
+    response = llm.invoke("Two most important ingredients in samosa are ")
     print(response.content)
-
-
 
 
 
